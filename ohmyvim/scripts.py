99451b3c4d32645e6f5cb81b2a07236491d96714
Gael Pasgrimaud <gael@gawel.org>
2011-12-11 04:21:52 +0100

    improve script

--- ohmyvim/scripts.py
+++ ohmyvim/scripts.py
@@ -14,6 +14,16 @@ import shutil
 import sys
 import os
 
+VIMRC = '''
+" added by oh-my-vim
+let g:ohmyvim="%(binary)s"
+
+" Use :OhMyVim profiles to list all available profiles
+let profiles = ['defaults']
+
+" load oh-my-vim
+source %(ohmyvim)s
+'''
 
 class Manager(object):
 
@@ -42,19 +52,16 @@ class Manager(object):
                                                    'autoload', 'pathogen.vim'))
                 fd.write('call pathogen#runtime_append_all_bundles()\n')
                 fd.write('source %s\n' % join(self.ohmyvim, 'theme.vim'))
-        source = ':source %s\n' % join(self.ohmyvim, 'ohmyvim.vim')
+        ohmyvim = join(self.ohmyvim, 'ohmyvim.vim')
+        binary = os.path.abspath(sys.argv[0])
         if not isfile(expanduser('~/.vimrc')):
             with open(expanduser('~/.vimrc'), 'w') as fd:
-                fd.write('\n" added by oh-my-zsh\n')
-                fd.write('let g:ohmyvim="%s"\n' % os.path.abspath(sys.argv[0]))
-                fd.write(source)
+                fd.write(VIMRC % locals())
         else:
             with open(expanduser('~/.vimrc')) as fd:
-                if source not in fd.read():
+                if binary not in fd.read():
                     with open(expanduser('~/.vimrc'), 'a') as fd:
-                        fd.write('\n" added by oh-my-zsh\n')
-                        fd.write('let g:ohmyvim="%s"\n' % abspath(sys.argv[0]))
-                        fd.write(source)
+                        fd.write(VIMRC % locals())
 
     def get_plugins(self):
         plugins = []
@@ -92,6 +99,7 @@ class Manager(object):
     def install_url(self, url):
         url = url.strip()
         dependencies = []
+        dirname = None
         if '://github.com' in url and not url.endswith('.git'):
             url = url.replace('http://', 'https://').rstrip() + '.git'
         if url.endswith('.git'):
@@ -109,7 +117,7 @@ class Manager(object):
                     dependencies = [d for d in fd.readlines()]
         else:
             print '%s is not a git url' % url
-        return dependencies
+        return dirname, dependencies
 
     def install(self, args):
         dependencies = set()
@@ -122,7 +130,7 @@ class Manager(object):
                     fd = urlopen(url)
                     dependencies = [d for d in fd.readlines()]
             else:
-                for d in self.install_url(url):
+                for _, d in self.install_url(url):
                     if d.strip():
                         dependencies.add(d)
         if dependencies:
@@ -151,11 +159,20 @@ class Manager(object):
     def theme(self, args):
         theme = args.theme
         if theme:
-            for plugin, dirname, themes in self.get_plugins():
-                if theme in themes:
-                    print 'Activate %s theme...' % theme
-                    with open(join(self.ohmyvim, 'theme.vim'), 'w') as fd:
-                        fd.write(':colo %s\n' % theme)
+            if theme.startswith('http'):
+                theme_dir, _ = self.install_url(theme)
+                for plugin, dirname, themes in self.get_plugins():
+                    if theme_dir == dirname and len(themes) == 1:
+                        theme = themes[0]
+                        print 'Activate %s theme...' % theme
+                        with open(join(self.ohmyvim, 'theme.vim'), 'w') as fd:
+                            fd.write(':colo %s\n' % theme)
+            else:
+                for plugin, dirname, themes in self.get_plugins():
+                    if theme in themes:
+                        print 'Activate %s theme...' % theme
+                        with open(join(self.ohmyvim, 'theme.vim'), 'w') as fd:
+                            fd.write(':colo %s\n' % theme)
             return
         for plugin, dirname, themes in self.get_plugins():
             if isdir(join(dirname, '.git')):
@@ -168,6 +185,22 @@ class Manager(object):
                 print '* %s (%s)' % (plugin, remote)
                 print '\t- %s' % ', '.join(themes)
 
+    def profiles(self, args):
+        profiles = join(self.runtime, 'oh-my-vim', 'profiles')
+        profiles = [join(profiles, p) for p in os.listdir(profiles)]
+
+        for profile in sorted(profiles):
+            name = basename(profile)[:-4]
+            desc = ''
+            with open(profile) as fd:
+                line = fd.readline()
+                if line.startswith('"'):
+                    desc += line.strip(' "\n')
+            if desc:
+                print '* %s - %s' % (name, desc)
+            else:
+                print '* %s' % name
+
 
 def main(*args):
     manager = Manager()
@@ -175,14 +208,14 @@ def main(*args):
     parser = argparse.ArgumentParser(description='Oh my Vim!')
     subparsers = parser.add_subparsers(help='sub-command help')
 
-    p = subparsers.add_parser('list')
-    p.add_argument('-u', '--urls', action='store_true', default=False)
-    p.set_defaults(action=manager.list)
-
     p = subparsers.add_parser('search')
     p.add_argument('-t', '--theme-only', action='store_true', default=False)
     p.add_argument('term', nargs='*', default='')
     p.set_defaults(action=manager.search)
+
+    p = subparsers.add_parser('list')
+    p.add_argument('-u', '--urls', action='store_true', default=False)
+    p.set_defaults(action=manager.list)
 
     p = subparsers.add_parser('install', help='install a script or bundle')
     p.add_argument('url', nargs='*', default='')
@@ -199,6 +232,9 @@ def main(*args):
     p = subparsers.add_parser('theme', help='list or activate a theme')
     p.add_argument('theme', nargs='?', default='')
     p.set_defaults(action=manager.theme)
+
+    p = subparsers.add_parser('profiles', help='list all available profiles')
+    p.set_defaults(action=manager.profiles)
 
     if args:
         args = parser.parse_args(args)
